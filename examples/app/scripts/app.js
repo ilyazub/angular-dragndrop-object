18ef4d84fbf26d1b7841611e86da4ecc2aa519c3
Illia Zub <zaoooza92@gmail.com>
2014-09-24 15:06:06 +0300

    Add jQuery as dependency

--- examples/app/scripts/app.js
+++ examples/app/scripts/app.js
@@ -1,5 +1,36 @@
 'use strict';
 
-angular.module('onoffClientApp', [
-  'angular-dragndrop-object'
+angular.module('example', [
+  'ilyazub.dragndrop-object'
 ]);
+
+angular
+  .module('example')
+  .controller('ExampleCtrl', function ($scope) {
+    $scope.stringify = function (obj) {
+      JSON.stringify(obj);
+    };
+
+    $scope.addToCart = function (fruit) {
+      $scope.basket.push(fruit);
+    }
+
+    $scope.fruits = [
+      {
+        id: 1,
+        title: 'Apple'
+      },
+      {
+        id: 2,
+        title: 'Chokeberry'
+      },
+      {
+        id: 3,
+        title: 'Hawthorn'
+      },
+      {
+        id: 4,
+        title: 'Serviceberry'
+      }
+    ]
+  });
